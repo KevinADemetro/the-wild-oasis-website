4b600687e4b3db89396d46d2458dfbaca31f2755
Kevin Demetro <albinodemetrok@gmail.com>
2025-07-05 16:45:26 -0300

    Adiciona favicon titulo e meta

--- app/layout.js
+++ app/layout.js
@@ -3,13 +3,18 @@ import Navigation from "./_components/Navigation";
 import "@/app/_styles/globals.css";
 
 export const metadata = {
-  title: "The Wild Oasis",
+  title: {
+    template: "%s / The Wild Oasis",
+    default: "Welcome / The Wild Oasis",
+  },
+  description:
+    "Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-gray-50">
+      <body className="bg-primary-950 text-primary-100">
         <header>
           <Logo />
           <Navigation />
